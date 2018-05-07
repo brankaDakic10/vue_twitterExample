9b148df843aa0d6873b094afcd90c8474b38b159
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-07 21:23:22 +0200

    Added new examples

---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+       <input type="text" v-model="password" />
+       <p>{{ status }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+           password: '',
+           isWeak: true
+        }
+    },
+    methods: {
+      
+    },
+    computed: {
+        status() {
+            return this.isWeak ? 'Weak' : 'Strong'
+        }
+    },
+
+   watch: {
+        password(newValue) {
+            if(newValue.length > 6 && /\d/.test(newValue)) {
+                this.isWeak = false
+            } else {
+                this.isWeak = true;
+            }
+        }
+    }
+  
+}
+</script>
---
+++ src/components/Twitterinput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <textarea v-model="text" @keydown='handleChange'></textarea>
+     <p>{{ numberOfCharacters}}/{{ maxNumberOfCharacters }}</p>
+  </div>
+</template>
+
+<script>
+const MAX_NUMBER_OF_CHARACTERS = 120
+
+
+export default {
+ data(){
+   return{
+     text:'',
+     maxNumberOfCharacters: MAX_NUMBER_OF_CHARACTERS
+   }
+ },
+ methods:{
+   handleChange(event){
+     
+     if(this.numberOfCharacters >= this.maxNumberOfCharacters){
+      // this line works with @input on textarea
+      //  this.text= this.text.slice(0, this.maxNumberOfCharacters)
+       event.preventDefault()
+     }
+   }
+ },
+ computed:{
+   numberOfCharacters(){
+     return this.text.length
+   }
+ }
+ 
+}
+</script>
+
+
+
