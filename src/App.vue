b5df270dcf0c60c43412e192b28305204ca51f70
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-10 19:29:26 +0200

    Added examples  with slotes and filters

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,81 @@
   <div id="app">
    
     <!-- <Twitterinput /> -->
-    <PasswordInput/>
+    <!-- <PasswordInput/> -->
+
+
+    <!-- example with filter -->
+    <article>{{name | allCaps|lowerCase |reverse |currency|join(name,city)}}</article>
+
+
+
+    <app-test>
+       <!-- 1.example -->
+      <!-- <p>This goes into slot</p>
+      <div>Slot</div>
+      <div>Name: {{name}}</div> -->
+        <!-- 2.example -->
+      <!-- <p>ASD</p> -->
+       <!-- 3.example -->
+       <!-- <div slot="header">Header</div>  
+
+      <div slot="footer">Footer</div> -->
+      <div slot-scope="slotProps">
+        {{slotProps.color}}
+        <!-- example with second props must be in the same div -->
+        {{slotProps.lastName}}
+      </div>
+    </app-test>
   </div>
 </template>
 
 <script>
 // import Twitterinput from './components/Twitterinput.vue'
 import PasswordInput from './components/PasswordInput.vue'
+import AppTest from './components/AppTest.vue'
+
 export default {
   name: 'app',
   components: {
     // Twitterinput,
-    PasswordInput
+    PasswordInput,
+    AppTest
+  },
+   data(){
+      return{
+           name:"Branka",
+           city:"Novi Sad"
+      }
+  },
+  filters:{
+    allCaps(value){
+      if( !value ){
+        return ""
+      }
+      return value.toString().toUpperCase()
+    },
+     lowerCase(value){
+      if( !value ){
+        return ""
+      }
+      return value.toString().toLowerCase()
+    },
+    reverse(value){
+      if(!value){
+        return ""
+      }
+      return value.split('').reverse().join('')
+    },
+    currency(value){
+      if(!value){
+        return ""
+      }
+      return '$'+value 
+    },
+    // this value is previous result,arg1 is pased name,arg2 is city
+    join(value,arg1,arg2){
+      return `${value}+${arg1}+${arg2}`
+    }
   }
 }
 </script>
---
+++ src/components/AppTest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+<!-- <slot>
+    <p>Some text from the parent here.</p>
+    </slot> -->
+<!-- <slot name="header"></slot>
+<slot name="footer"></slot> -->
+<slot :color="color" :lastName="lastName"></slot>
+<!-- example with second props  -->
+
+  </div>
+</template>
+<script>
+export default {
+ data(){
+     return{
+         color:"Blue",
+         lastName:"Dakic"
+     }
+ }
+}
+</script>
